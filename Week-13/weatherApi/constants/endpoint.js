324c433c93fccf3fa5c3c93bdc62665b34d0887f
Suresh Konar <SKonar@evolenthealth.com>
2022-10-29 13:55:47 +0530

    added code change for week 13 assignment

---
+++ Week-13/weatherApi/constants/endpoint.js
@@ -0,0 +1,12 @@
+export const BASE_URL = "https://api.weatherapi.com/v1/";
+
+export const WEATHER_API = {
+  CURRENT: "current.json",
+  FORECAST: "forecast.json",
+};
+
+export const ALLOWED_APIS = {
+  GET_MULTIPLE_WEATHER: "/getmultiplecityweather",
+  GET_CURRENT_WEATHER: "/getcurrentweather",
+  GET_FORECAST: "/getforecast",
+};
