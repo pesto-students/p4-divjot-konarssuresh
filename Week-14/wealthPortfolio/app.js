a1e81c7f4578dde7cbfcacf673ab0a26e8b9e0e9
Suresh Konar <SKonar@evolenthealth.com>
2022-11-14 14:53:07 +0530

    added code to add/update/delete expense

--- Week-14/wealthPortfolio/app.js
+++ Week-14/wealthPortfolio/app.js
@@ -1,17 +1,29 @@
 import express from "express";
 import {userRouter} from "./routes/userRouter.js";
+import {expenseRouter} from "./routes/expenseRouter.js";
+import morgan from "morgan";
+import fs from "fs";
+import path, {dirname} from "path";
+import {fileURLToPath} from "url";
 
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 let app = express();
 
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  {
+    flags: "a",
+  }
+);
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
-
+app.use(morgan("combined", {stream: accessLogStream}));
 app.use("/user", userRouter);
-
+app.use("/expense", expenseRouter);
 app.use((err, res) => {
-  console.log("middleware reached");
   if (err) {
-    console.log("internal server error occured");
+    console.log(err);
     res.status(500).json({message: "Internal Server Error Occured"});
   }
 });
--- Week-14/wealthPortfolio/controller/userController.js
+++ Week-14/wealthPortfolio/controller/userController.js
@@ -1,26 +1,33 @@
 import {MongoClient} from "mongodb";
 import {MONGO_DB} from "../constants/endpoint.js";
 import lodash from "lodash";
+import {UUID} from "bson";
 const {isEmpty} = lodash;
 const signUpUser = async (req, res) => {
-  const client = new MongoClient(MONGO_DB.CONNECTION);
+  const client = new MongoClient(MONGO_DB.CONNECTION, {
+    useUnifiedTopology: true,
+    pkFactory: {createPk: () => new UUID().toBinary()},
+  });
   try {
     const usersCollection = client.db("wealthManager").collection("users");
     const {userId, ...remainingProps} = req.body;
     await client.connect();
     const userCursor = await usersCollection.find({
-      userId,
+      _id: userId,
     });
     const users = await userCursor.toArray();
     if (isEmpty(users)) {
-      const userDoc = {...remainingProps, userId, expenses: [], incomes: []};
+      const userDoc = {
+        ...remainingProps,
+        _id: userId,
+        expenses: [],
+        incomes: [],
+      };
       await usersCollection.insertOne(userDoc);
       res.status(200).json({message: "User added successfully"});
     } else {
       res.status(400).json({errors: ["UserId already exists"]});
     }
-  } catch (e) {
-    throw e;
   } finally {
     await client.close();
   }
@@ -33,7 +40,7 @@ const loginUser = async (req, res) => {
     const {userId, password} = req.body;
     await client.connect();
     const userCursor = await usersCollection.find({
-      userId,
+      _id: userId,
     });
     const users = await userCursor.toArray();
     if (isEmpty(users)) {
@@ -45,8 +52,6 @@ const loginUser = async (req, res) => {
         res.status(200).json({message: "User logged in successfully"});
       }
     }
-  } catch (e) {
-    throw e;
   } finally {
     await client.close();
   }
